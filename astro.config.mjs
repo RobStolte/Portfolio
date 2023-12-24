57678a400c43113c49ea172c0c9c60c69c463e19
Pomp0m1989 <46743058+RobStolte@users.noreply.github.com>
2023-12-24 16:18:16 +0100

    push preworks storyblocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,27 @@
+/** @format */
+
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-import storyblok from "@storyblok/astro";
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import tailwind from '@astrojs/tailwind';
+import storyblok from '@storyblok/astro';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
 
-import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: 'https://robstolte.nl',
   integrations: [
+    sentry({
+      dsn: "https://5a29e2ff95364190217e762626d3c36a@o4506437940281344.ingest.sentry.io/4506437943558144",
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+    spotlightjs(),
     mdx(),
     sitemap(),
     tailwind(),
@@ -19,28 +29,26 @@ export default defineConfig({
       accessToken: process.env.STORYBLOK_TOKEN,
       bridge: true,
       apiOptions: {
-        region: "eu",
-      }, // storyblok-js-client options
-      components: {
-        home: "pages/home",
-        page: "storyblok/Page",
-        grid: "storyblok/Grid",
-        feature: "storyblok/Feature",
-        project: "storyblok/Project",
-        blogpost: "storyblok/BlogPost",
+        region: 'eu',
       },
-      componentsDir: "src",
+      // storyblok-js-client options
+      components: {
+        home: 'pages/home',
+        page: 'storyblok/Page',
+        grid: 'storyblok/Grid',
+        feature: 'storyblok/Feature',
+        project: 'storyblok/Project',
+        blogpost: 'storyblok/BlogPost',
+      },
+      componentsDir: 'src',
       enableFallbackComponent: true,
       useCustomApi: false,
     }),
-
-   //sentry(),
-   //spotlightjs()
   ],
   vite: {
     plugins: [basicSsl()],
     server: {
       https: true,
-    }
-  }
+    },
+  },
 });
