92d0a762505348569fcd9c9250b1984d67b7fac2
Pomp0m1989 <46743058+RobStolte@users.noreply.github.com>
2023-12-26 15:59:43 +0100

    big update

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,24 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+export const PERSOONLIJKE_INFO = {
+    Voornaam: "Rob",
+    Achternaam: "Stolte",
+    Geboortejaar: 1989,
+    GeboorteMaand: 12,
+    GeboorteDag: 27,
+    Email: "contact@robstolte.nl"
+}
+export const SIDE_BAR_MENU = [
+    { id: 'home', icon: 'fas fa-house-user', slug: '' },
+    { id: 'projecten', icon: 'fab fa-product-hunt', slug: 'projecten' },
+    { id: 'blog', icon: 'fab fa-blogger-b', slug: 'blog' },
+        { id: 'cv', icon: 'fas fa-list-check', slug: 'cv' }
+    ];
+    export const CONTACT_METHODS = [
+        { id: 'email', icon: 'fas fa-envelope', url: `mailto:${PERSOONLIJKE_INFO.Email}`, ariaLabel:'Email', titel: 'Stuur me een mailtje!' },
+        { id: 'linkedin', icon: 'fab fa-linkedin', url: 'https://www.linkedin.com/in/rob-stolte/', ariaLabel:'LinkedIn', titel: 'Bekijk mijn LinkedIn profiel!' },
+        { id: 'github', icon: 'fab fa-github', url: 'https://github.com/RobStolte', ariaLabel:'GitHub', titel: 'Bekijk mijn GitHub profiel!' },
+        { id: 'twitter', icon: 'fab fa-twitter', url: 'https://twitter.com/pomp0m', ariaLabel:'Twitter', titel: 'Bekijk mijn Twitter profiel!' },
+        { id: 'paypal', icon: 'fab fa-paypal', url: 'https://paypal.me/RobStolte', ariaLabel:'PayPal', titel: 'Doneer via PayPal!' }
+    ]
 
-export const SITE_TITLE = 'Portfolio | Rob Stolte';
-export const SITE_DESCRIPTION = 'Een portfolio website van Rob Stolte, waar je meer informatie kan vinden over mijn werk en hobby\'s. Ook kan je hier mijn contactgegevens vinden.';
+export const SITE_TITLE = `Portfolio | ${PERSOONLIJKE_INFO.Voornaam} ${PERSOONLIJKE_INFO.Achternaam}`;
+export const SITE_DESCRIPTION = `Een portfolio website van ${PERSOONLIJKE_INFO.Voornaam} ${PERSOONLIJKE_INFO.Achternaam}, waar je meer informatie kan vinden over mijn werk en hobby\'s. Ook kan je hier mijn contactgegevens vinden.`;
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,24 @@
+import rss from "@astrojs/rss";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
+import { getCollection } from "astro:content";
+
+export async function GET(context) {
+  const blog = await getCollection("blog");
+  const projecten = await getCollection("projecten");
+
+  const combined = [...blog, ...projecten];
+
+  const items = combined.map((post) => ({
+    title: post.data.title,
+    pubDate: post.data.pubDate,
+    description: post.data.description,
+    link: `/${post.collection}/${post.slug}/`,
+  }));
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: import.meta.env.SITE,
+    items: items,
+  });
+}
