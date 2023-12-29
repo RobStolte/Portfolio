7dec500c3f676163dd6cb559fccab6d142b5ece6
Pomp0m1989 <46743058+RobStolte@users.noreply.github.com>
2023-12-29 14:45:50 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,27 +10,18 @@ import sentry from '@sentry/astro';
 import spotlightjs from '@spotlightjs/astro';
 import partytown from "@astrojs/partytown";
 import vue from "@astrojs/vue";
-import { loadEnv } from "vite";
-const {
-  STORYBLOK_TOKEN,
-  SENTRY_AUTH_TOKEN,
-  SENTRY_DNS_URL
-} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+//  import { loadEnv } from "vite";
+//  const {
+//    STORYBLOK_TOKEN,
+//    SENTRY_AUTH_TOKEN,
+//    SENTRY_DNS_URL
+//  } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 
 // https://astro.build/config
 export default defineConfig({
-  root: '.',
-  srcDir: './src',
-  publicDir: './public',
-  outDir: './dist',
   cacheDir: './.cache',
   site: 'https://robstolte.nl',
-
-  server: {
-    port: 1234,
-    host: true
-  },
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'hover',
@@ -40,31 +31,36 @@ export default defineConfig({
     format: 'file',
     inlineStylesheets: 'never'
   },
-  integrations: [sentry({
-    dsn: SENTRY_DNS_URL,
-    sourceMapsUploadOptions: {
-      project: "portfolio",
-      authToken: SENTRY_AUTH_TOKEN
-    }
-  }), spotlightjs(), mdx(), sitemap(), tailwind(), storyblok({
-    accessToken: STORYBLOK_TOKEN,
-    bridge: true,
-    apiOptions: {
-      region: 'eu'
-    },
-    // storyblok-js-client options
-    components: {
-      //home: 'pages/home',
-      //page: 'storyblok/Page',
-      //grid: 'storyblok/Grid',
-      //feature: 'storyblok/Feature',
-      //project: 'storyblok/Project',
-      //blogpost: 'storyblok/BlogPost',
-    },
-    componentsDir: 'src',
-    enableFallbackComponent: true,
-    useCustomApi: false
-  }), vue(), partytown()],
+  integrations: [
+  spotlightjs(), mdx(), sitemap(), tailwind(),
+  vue(), partytown(),
+    //sentry({
+    //  dsn: SENTRY_DNS_URL,
+    //  sourceMapsUploadOptions: {
+    //    project: "portfolio",
+    //    authToken: SENTRY_AUTH_TOKEN
+    //  }
+    //}),
+    //storyblok({
+    //  accessToken: STORYBLOK_TOKEN,
+    //  bridge: true,
+    //  apiOptions: {
+    //    region: 'eu'
+    //  },
+    //  // storyblok-js-client options
+    //  components: {
+    //    //home: 'pages/home',
+    //    //page: 'storyblok/Page',
+    //    //grid: 'storyblok/Grid',
+    //    //feature: 'storyblok/Feature',
+    //    //project: 'storyblok/Project',
+    //    //blogpost: 'storyblok/BlogPost',
+    //  },
+    //  componentsDir: 'src',
+    //  enableFallbackComponent: true,
+    //  useCustomApi: false
+    //}),
+  ],
   vite: {
     plugins: [basicSsl()],
     server: {
--- src/content/blog/eerste-week-xperia-1v.md
+++ src/content/blog/eerste-week-xperia-1v.md
@@ -2,7 +2,7 @@
 title: Eerste week met sony Xperia 1 V
 description: De eerste week met de nieuwe Sony Xperia 1 V is voorbij, en ik ben er erg tevreden mee. De telefoon is snel, heeft een goede camera en een mooi scherm.
 pubDate: 2023-07-18T17:25:00.000Z
-heroImage: '../../assets/blog/eerste-week-xperia-1v/BekendelleMolen.webp'
+heroImage: '@/assets/blog/eerste-week-xperia-1v/BekendelleMolen.webp'
 slug: eerste-week-met-sony-xperia-1
 ---
 
@@ -10,12 +10,12 @@ Na een weekje te lopen met de nieuwe telefoon merk je dat het first and foremost
 
 Het is echt mijn onervarenheid dat de foto's minder picturesque maakt maar de scenes die er komen zijn wel erg goede kwaliteit. De camera doet binnen zijn parameters dus echt geweldig werk. Het scherm is echter erg lastig met het laten zien van de focus van de camera. Het is erg lastig om te zien of de foto scherp is of niet. Dit is wel een beetje jammer. Voorral omdat de focuspeeking wel laat denken dat de focus goed loopt. Dit is goed te zien in de foto hieronder. waar de railing in focus is maar de focuspeeking dit niet duidelijk liet zien.
 
-![Foto van de bekendelle molen](../../assets/blog/eerste-week-xperia-1v/BekendelleDam.webp)
+![Foto van de bekendelle molen](@/assets/blog/eerste-week-xperia-1v/BekendelleDam.webp)
 
 De scenes heb ik naderhand ook nog een beetje mee zitten spelen en het kleurpalette is erg breed maar heeft bijna altijd een mooie realistische ondertoon, met uitzondering van 'vibrant'S
 
 Onsterstaande foto is met alle instellingen op manual zodat de foto's alleen op de ingebouwde lut een ander kleurbeeld geven. De foto is gemaakt in de bossen achter de bekendelle molen.
-![Foto van de bekendelle molen](../../assets/blog/eerste-week-xperia-1v/loop.webp)
+![Foto van de bekendelle molen](@/assets/blog/eerste-week-xperia-1v/loop.webp)
 
 De videomodus is echt wel verbluffend
 
