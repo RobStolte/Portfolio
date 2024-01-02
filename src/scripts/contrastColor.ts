87f610f3bf7c0f7c8e8a5cceefe8be0774853526
Pomp0m1989 <46743058+RobStolte@users.noreply.github.com>
2024-01-02 15:53:42 +0100

    allot of reducing of files

--- src/scripts/contrastColor.ts
+++
@@ -1,8 +0,0 @@
-export default function contrastColor(inputColor: string) {
-    const color = (inputColor.charAt(0) === '#') ? inputColor.substring(1, 7) : inputColor;
-    const r = parseInt(color.substring(0, 2), 16); // red
-    const g = parseInt(color.substring(2, 4), 16); // green
-    const b = parseInt(color.substring(4, 6), 16); // blue
-    const brightness = (r * 299 + g * 587 + b * 114) / 1000;
-    return brightness > 128 ? 'black' : 'white';
-  }
--- src/scripts/getAge.ts
+++
@@ -1,5 +0,0 @@
-export default (y: number, m: number, d: number) => {
-    const t = new Date(), b = new Date(y, m - 1, d);
-    let a = t.getFullYear() - b.getFullYear();
-    return t.getMonth() - b.getMonth() < 0 || (t.getMonth() - b.getMonth() === 0 && t.getDate() < b.getDate()) ? --a : a;
-  }
