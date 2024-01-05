ab78ec128b9f56c95ce50a703a2b20f54cddf52b
Pomp0m1989 <46743058+RobStolte@users.noreply.github.com>
2024-01-05 12:29:39 +0100

    added copyright and language

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,8 @@
 import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "@/config";
 import { useStoryblokApi } from '@storyblok/astro'
+import { PERSOONLIJKE_INFO } from "@/config.ts";
+const today = new Date();
 const storyblokApi = useStoryblokApi()
 
 interface Story {
@@ -21,13 +23,15 @@ export async function GET() {
   const { data } = await storyblokApi.get('cdn/stories', {
     version: import.meta.env.DEV ? "draft" : "published",
   })
-
   const items = data.stories.map((story: Story) => ({
+
     title: story.name,
     pubDate: story.first_published_at,
     description: story.content.description,
     link: story.full_slug,
     customData: `
+    <copyright>${today.getFullYear()} ${PERSOONLIJKE_INFO.Voornaam} ${PERSOONLIJKE_INFO.Achternaam} All rights reserved.</copyright>
+    <language>nl-nl</language>
     <image>
       <url>${story.content.heroImage.filename}/m/0x144/filters:format(png)</url>
       <width>144</width>
@@ -35,7 +39,6 @@ export async function GET() {
       <link>${story.full_slug}</link>
     </image>`,
   }));
-
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
