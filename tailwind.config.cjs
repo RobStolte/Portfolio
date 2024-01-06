f19a5cf94281d522eb632bef59094346f2ef2b8e
Pomp0m1989 <46743058+RobStolte@users.noreply.github.com>
2024-01-06 10:53:45 +0100

    some style changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,26 @@
 module.exports = {
 	content: ['src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				'xl': '0 0px 8px 3px rgb(0 0 0 / 0.25)',
+				'2xl': '0 0px 22px 15px rgb(0 0 0 / 0.25)',
+				'4xl': '0 0px 40px 20px rgb(0 0 0 / 0.25)',
+			},
+			borderRadius: {
+				'2xl': '1.5rem',
+			},
+		},
+
 	},
-	plugins: [require("@tailwindcss/typography"),require("daisyui")],
+	plugins: [
+		require("@tailwindcss/typography"),
+		require("daisyui")],
 	daisyui: {
 		themes: false, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "dark", // name of one of the included themes for dark mode
-		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+		base: true,
+		styled: true,
+		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 	  }
 }
